9ffddc3001ac4f4aa0b174f2a21118c2e6941a61
Barbara Betti <barbarabetti98@libero.it>
2023-02-07 14:03:45 +0100

    Co-authored-by: Leo <leokayser@users.noreply.github.com>

--- fun.jl
+++ fun.jl
@@ -1,5 +1,19 @@
+using Oscar
 
 using HomotopyContinuation
 
+@var x[0:5]
 
+#R = PolynomialRing(QQ, x)
 
+#var(R)
+
+G = grassmann_pluecker_ideal(2, 4)
+L = [rand(-100:100,6)'*x for i = 1:4]
+
+f=vcat(gens(G),L)
+
+typeof(gen)
+
+variables(f)
+F = System(f; variables = x)  
