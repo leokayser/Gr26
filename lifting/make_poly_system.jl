be48e499cbcdc9f8eacadc8ba45a887022659db3
leokayser <87534843+LeoKayser@users.noreply.github.com>
2024-02-23 17:47:09 +0100

    Composing systems

--- lifting/make_poly_system.jl
+++ lifting/make_poly_system.jl
@@ -1,45 +1,59 @@
 #include("../Utilities.jl")
 include("make_start_system.jl")
 
+
 S_start, φ_start = make_start();
 
 
-@var a[1:3,1:7]
-@var s[1:21], s_target[1:21]
-@var t
+@var a[1:21]
+@var s[1:21]
+@var x[1:7]
+@var p[1:15]
 
 S, s_oscar = PolynomialRing(QQ, ["s$i" for i=1:21])
 
 # s -> skew matrix
-s_t = t*S_start + (1-t)*s_target
+#s_t = t*S_start + (1-t)*s_target
 
 # skew matrix -> O (poor man's cayley using adjunct)
-O_t = fake_Cayley_polynomial(7,s_oscar);
+OO_t = fake_Cayley_polynomial(7,s_oscar);
 
 # O -> \Gamma = [I_7 | O]
-Γ_oscar = hcat(Matrix(identity_matrix(S,7)), O_t );
+Γ_oscar = hcat(Matrix(identity_matrix(S,7)), OO_t );
 Γ =  [oscar_to_HC_Q(Γ_oscar_entry, s) for Γ_oscar_entry in Γ_oscar];
+Γ_sys = [System(vcat(Γ[:,i],a), variables=[s;a]) for i=1:14]
 
 # a -> \varphi_a (using \varphi_start[1:12,:] from before)
-# Need to load \varphi_start from make_start_system
 
-φ_a = vcat(φ_start[1:12,:], a)
+φ_a = vcat(φ_start[1:12,:], reshape(a,3,7))
+φ_sys = System(φ_a*x, variables=[x;a])
 
-# Z = \varphi_a * \Gamma
 
-Z = φ_a * Γ;
+#Z_sys = [HomotopyContinuation.compose(φ_sys,Γ_sys[i]) for i=1:14];
+
+#Z = φ_a * Γ;
+
+#Z_sys = System(reshape(Z,210), variables=[s[:];a[:]]);
 
 # p_1..15 = plücker relations
-Pluecker_rel = gens( grassmann_pluecker_ideal(2,6))
+plück_oscar = gens( grassmann_pluecker_ideal(2,6))
 
-plück_num = poly_to_fp(Pluecker_rel)
+plück_sys = System([oscar_to_HC_Q(plück_oscar[i], p) for i=1:15], variables=p)
+
+plück_φ_sys = HomotopyContinuation.compose(plück_sys,φ_sys)  
+total_sys = [ HomotopyContinuation.compose(plück_φ_sys, Γ_sys[i]) for i=1:14  ]
+# total_sys is a vector of Systems. 
+# We need to concatenate all the equations in a unique system keeping the same 'a' variables and different 'x'
+
+
 
 # f = p_j(Z_i) for i=1..14, j=1..15
-system = hcat([plück_num(Z[:,i]) for i=1:14]...);
-length(system) #210
+#total_sys = [HomotopyContinuation.compose(plück_sys, Z_sys[i]) for i=1:14];
+#length(system) #210
 
 
-#C = System(system, variables = a, parameters = )
+
+C = System(system_hc, variables = a[1:21], parameters = s[1:21])
 
 
 
