00c8295092b945ffcccb98f80fb48d0396ab7f75
leokayser <87534843+LeoKayser@users.noreply.github.com>
2024-05-06 17:49:55 +0200

    Panic, Trackers, fun

--- lifting/make_poly_system.jl
+++ lifting/make_poly_system.jl
@@ -5,7 +5,7 @@ include("make_start_system.jl")
 S_start, L_start = make_start();
 
 
-@var l[1:21]
+@var l[1:22]
 @var s[1:21]
 @var x[1:7]
 @var q[1:15]
@@ -15,8 +15,6 @@ S, s_oscar = polynomial_ring(QQ, ["s$i" for i=1:21])
 # s -> skew matrix
 #s_t = t*S_start + (1-t)*s_target
 
-# skew matrix -> O (poor man's cayley using adjunct)
-#OO_t = fake_Cayley_polynomial(7,s_oscar);
 OscarSkewMat = skew_matrix(s_oscar)
 Smat = [oscar_to_HC_Q(m, s) for m in OscarSkewMat]
 
@@ -29,7 +27,18 @@ Smat = [oscar_to_HC_Q(m, s) for m in OscarSkewMat]
 A_rand = [randn(ComplexF64,15,7) for _ in eachindex(l) ]
 
 L_l = L_start + sum(l[i]*A_rand[i] for i in eachindex(l))
-L_sys = System(L_l*x, variables=[x;l])
+
+#indices = vcat([[i,1] for i in 2:15],[[i,2] for i in 2:9])
+
+#L_l = Matrix{Any}(copy(L_start));
+#for i in eachindex(l)
+#    L_l[indices[i]...] = l[i]
+#end
+
+
+
+
+L_sys = System(L_l*x, variables=[x;l]);
 
 
 # q_1..15 = plücker relations
@@ -43,20 +52,30 @@ Q_sys = System(Q, variables=[x;l]);
 equations = vcat([Q_sys([Γ[:,i];l]) for i in 1:14]...);
 
 
-parameterized_system = System(equations, variables=l, parameters=s);
+parametrized_system = System(equations, variables=l, parameters=s);
 
+#l_start = [L_start[indices[i]...] for i in eachindex(l)]
 
-l_start = zeros(ComplexF64,21)
-#parameterized_system(l_start,S_start)   # Also sanity check
+l_start = zeros(ComplexF64,22)
+parametrized_system(l_start,S_start)   # Also sanity check
 
-R = RandomizedSystem(parameterized_system, 21);
+#R = RandomizedSystem(parameterized_system, 21);
 
-S_target = rand(ComplexF64,21)
+S_target = randn(ComplexF64,21)
+
+tnf_homotopy = parameter_homotopy(InterpretedSystem(parametrized_system);
+    start_parameters = S_start, target_parameters = S_target);
+tracker = Tracker(tnf_homotopy; options = TrackerOptions(parameters = FAST_TRACKER_PARAMETERS) )
+result = track.(tracker, [l_start])
 
 println("Starting solve")
 
-@time result = HomotopyContinuation.solve(R, l_start; start_parameters=S_start, target_parameters=S_target)
+#@time result = HomotopyContinuation.solve(R, l_start; start_parameters=S_start, target_parameters=S_target)
+@time result = HomotopyContinuation.solve(
+    parametrized_system, l_start; start_parameters=S_start,
+    target_parameters=S_target, compile = false, show_progress = true)
 
 result.path_results
+sol = solution(result[1])
 
-norm(parameterized_system(solutions(result)[1], S_target), Inf)
+norm(parametrized_system(rand(ComplexF64,21), S_target), Inf)
