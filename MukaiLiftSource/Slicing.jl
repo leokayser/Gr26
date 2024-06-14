e1318875c8c2090fb799fbde5d27b43081d9e8c8
Barbara Betti <barbarabetti98@libero.it>
2024-06-14 15:22:22 +0200

    commenting Slicing.jl

--- MukaiLiftSource/Slicing.jl
+++ MukaiLiftSource/Slicing.jl
@@ -1,9 +1,10 @@
+# This file accompanies section 3 of the paper.
+
 using Pkg; Pkg.activate("MukaiLiftP6")
 using MukaiLiftP6
 using Oscar
 using LinearAlgebra
 using HomotopyContinuation
-
 
 R,p,_,_ = plückercoordinates(6,QQ);
 T,_ = polynomial_ring(QQ, vcat(["t$i" for i=1:8], "u" ) )
@@ -15,7 +16,11 @@ pu_HC = [toric_degen_poly_HC(p[i],w,[x;u],gens(T)[9]) for i=1:15]
 F = a*pu_HC
 C = System(F, variables = x, parameters = [u;a[:]])
 
-println("Defining system done.")
+# The system C is a Gröbner degeneration of the plücker.
+# u=1 describes the problem of slicing Gr(2,6) with the linear space defined by the kernel matrix a[:].
+# The following code block (commented out) computes a general complex starting solution to the slicing problem.
+# If you only want to slice Gr(2,6) you can just read the parameters from the files (below).
+
 #=
 A_rand    = randn(ComplexF64, length(a))
 
@@ -26,7 +31,6 @@ a1_start  = [1;A_rand]
 start_sol = solutions(HomotopyContinuation.solve(C, toric_sol;
     start_parameters = a0_start, target_parameters = a1_start))
 
-# given A
 A    = randn(ComplexF64, length(a))
 a1_target = [1;A]
 X8_cap_A = solutions(HomotopyContinuation.solve(C, start_sol;
@@ -36,29 +40,23 @@ HomotopyContinuation.write_parameters("Gr26_start_parameters.txt", a1_start)
 HomotopyContinuation.write_solutions("Gr26_start_system.txt", start_sol)
 =#
 
-
    
 a1_start = HomotopyContinuation.read_parameters("MukaiLiftP6/src/Gr26_start_parameters.txt")
 start_sol = HomotopyContinuation.read_solutions("MukaiLiftP6/src/Gr26_start_system.txt")
 
 
-########################
-
-# Check that we have obtained points on the grassmannian by evaluating pluecker relations at those points
-#numerical_plücker = poly_to_func(ϕ) 
-#Z = numerical_plücker.(gr_start_system) # 14 points in P^14
-
-#[A*z for z in Z] #the points in Z lies in the linear section of P^14 as they should
-
-
-########################
-
-println("Loading start parameters done!")
-
+# Sample a matrix with the same format as a, such that its column space is a uniformly random point of the respective real Grassmannian.
+# -------------  Input:
+# a            a matrix
+# -------------  Output:
+#              a matrix
 function uniform_Gr_point(a)
     return randn(length(a[:]))
 end
 
+# Sample a uniformly random real linear space from Gr(7,15) and count the number of real intersection points with Gr(2,6) in P^14
+# -------------  Output:
+#              a number, the real solution count or -1 if there are fewer than 14 nonsingular complex solutions
 function count_rand_sample()
     targ_par = [1; uniform_Gr_point(a)]
     slice_result = HomotopyContinuation.solve(C, start_sol; start_parameters = a1_start, target_parameters = targ_par, show_progress=false)
